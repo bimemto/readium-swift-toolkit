5f9b0a8492f76408cb776d0766c5fcc2adfbb5b2
Joey Nguyen <joey@Joeys-MacBook-Pro.local>
2026-03-16 20:51:25 +0700

    fix: total page

--- Sources/Navigator/EPUB/Scripts/src/utils.js
+++ Sources/Navigator/EPUB/Scripts/src/utils.js
@@ -102,7 +102,7 @@ function onScroll() {
     lastKnownProgressions = {
       first: scrollY / totalContentHeight,
       last: (scrollY + viewportHeight) / totalContentHeight,
-      pageCount: viewportHeight > 0 ? Math.max(1, Math.round(totalContentHeight / viewportHeight)) : 1,
+      pageCount: viewportHeight > 0 ? Math.max(1, Math.ceil(totalContentHeight / viewportHeight - 0.1)) : 1,
     };
   } else {
     let scrollX = window.scrollX;
