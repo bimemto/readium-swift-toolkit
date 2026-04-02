b33be6839c5a850e806e5166ccd6019bcd91839a
Joey Nguyen <joey@Joeys-MacBook-Pro.local>
2026-04-02 09:24:45 +0700

    add error page

--- Sources/Navigator/EPUB/EPUBSpreadView.swift
+++ Sources/Navigator/EPUB/EPUBSpreadView.swift
@@ -546,6 +546,7 @@ extension EPUBSpreadView: WKNavigationDelegate {
         let html = """
         <html>
         <head>
+            <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
         </head>
         <body style="
